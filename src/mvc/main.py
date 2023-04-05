8e8307ec60a08c2b839bd77446ef9522cb81e689
Diego Molina <diegomolina_30@hotmail.com>
2023-04-05 14:26:39 -0500

    add main file to run app

---
+++ src/mvc/main.py
@@ -0,0 +1,12 @@
+from PyQt5 import QtWidgets
+import sys
+from view.login import Ui_Form
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Form = QtWidgets.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(Form)
+    Form.show()
+    sys.exit(app.exec_())
