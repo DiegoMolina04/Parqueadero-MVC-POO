445c5215bc05fbbd53e052c2645e719e499a6482
Diego Molina <diegomolina_30@hotmail.com>
2023-04-04 21:57:38 -0500

    add Carro class

---
+++ src/mvc/model/Carro.py
@@ -0,0 +1,13 @@
+class Carro():
+
+    def __init__(self, placa:str, marca:str, modelo:str, color:str):
+        self.placa = placa
+        self.marca = marca
+        self.modelo = modelo
+        self.color = color
+
+    def arrancar():
+        pass
+
+    def estacionar():
+        pass
