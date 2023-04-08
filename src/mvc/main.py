a79f63877d60d822a3c6173e08edf8cef0dd7019
Diego Molina <diegomolina_30@hotmail.com>
2023-04-08 12:56:14 -0500

    change main

--- src/mvc/main.py
+++ src/mvc/main.py
@@ -1,13 +1,7 @@
-from PyQt5 import QtWidgets
-import sys
-from view.login import Ui_Form
 from controller.MainController import MainController
+from controller.LoginController import LoginController
 
-if __name__ == "__main__":
-    MainController.initElements()
-    app = QtWidgets.QApplication(sys.argv)
-    Form = QtWidgets.QWidget()
-    ui = Ui_Form()
-    ui.setupUi(Form)
-    Form.show()
-    sys.exit(app.exec_())
+MainController.iniciarDatos()
+
+loginController = LoginController()
+loginController.iniciarLogin()
