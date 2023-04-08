e2448ee0cec862bde7fe411410b41de925bd7cf9
Diego Molina <diegomolina_30@hotmail.com>
2023-04-08 13:02:27 -0500

    change name of method

--- src/mvc/controller/MainController.py
+++ src/mvc/controller/MainController.py
@@ -2,7 +2,7 @@ from model.Administrador import Administrador
 
 class MainController():
 
-    def initElements():
+    def iniciarDatos():
         admin1 = Administrador("correo@correo","1234",123456798,"Marcos")
         admin2 = Administrador("gmail@correo","ejem",78945,"Viviana")
-        print("Elementos inicializados")
+        print("Elementos inicializados")    
