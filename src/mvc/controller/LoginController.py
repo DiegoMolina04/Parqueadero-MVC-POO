0823576c4fe2c11b1ae9b5db068f28e396a43ccf
Diego Molina <diegomolina_30@hotmail.com>
2023-04-08 13:04:03 -0500

    add feature open & close windows

--- src/mvc/controller/LoginController.py
+++ src/mvc/controller/LoginController.py
@@ -1,18 +1,55 @@
 from model.Administrador import Administrador
+from PyQt5.QtWidgets import QMessageBox
+from PyQt5 import QtWidgets, QtGui, uic
+import os
 
 class LoginController():
 
-    def controller(self, correo:str, contraseña:str):
+    app = QtWidgets.QApplication([])
+    actualPath = os.getcwd()+"/src/mvc/view/login.ui"
+    login = uic.loadUi(actualPath)
+
+    def verificarDatos(correo:str, contraseña:str):
         
-        respuesta:bool = False
+        respuesta = [False, ""]
+        mensaje = []
 
         if len(correo and contraseña):
-            #respuesta = True
+
             respuesta = Administrador.iniciarSesion(correo,contraseña)
-            #admin = Administrador()
-            #admin.iniciarSesion(correo,contraseña)
+
+        if respuesta[0] != False and respuesta[1] != None:
             
-        
-        return respuesta
-        
-        #pass
+            mensaje = [f"¡Bienvenido al sistema {respuesta[1][1]}!", QMessageBox.Information]
+
+        elif respuesta[0] == False and respuesta[1] == "":
+
+            mensaje = ["Complete todos los campos", QMessageBox.Warning]
+
+        else:
+
+            mensaje = ["Verifique los datos ingresados", QMessageBox.Warning]
+
+        return mensaje
+
+    def iniciarLogin(self):
+
+        LoginController.login.buttonEnviar.clicked.connect(LoginController.generarCuadroDialogo)
+        LoginController.login.show()
+        LoginController.app.exec()
+
+    def generarCuadroDialogo(self):
+
+        correo = LoginController.login.inputCorreo.text()
+        contraseña = LoginController.login.inputContrasena.text()
+        respuesta = LoginController.verificarDatos(correo, contraseña)
+        ventana = QMessageBox()
+        ventana.setText(respuesta[0])
+        ventana.setIcon(respuesta[1])
+        ventana.setWindowIcon(QtGui.QIcon(":/images/icon.png"))
+        ventana.setWindowTitle("Parqueadero")
+        ventana.exec_()
+        LoginController.cerrarLogin()
+
+    def cerrarLogin():
+        LoginController.login.close()
