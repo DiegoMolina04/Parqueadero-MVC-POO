e4fbfaa7026958bed8ccabdc1eea2a73b1c415d1
Diego Molina <diegomolina_30@hotmail.com>
2023-04-11 23:01:27 -0500

    update MainController init Parqueadero

--- src/mvc/controller/MainController.py
+++ src/mvc/controller/MainController.py
@@ -1,8 +1,12 @@
 from model.Administrador import Administrador
+from model.Parqueadero import Parqueadero
 
 class MainController():
 
     def iniciarDatos():
         admin1 = Administrador("correo@correo","1234",123456798,"Marcos")
-        admin2 = Administrador("gmail@correo","ejem",78945,"Viviana")
-        print("Elementos inicializados")    
+        admin2 = Administrador("a","a",78945,"Viviana")
+        parqueadero = Parqueadero(2, 5)
+        #parqueadero = Parqueadero(1, 1)
+        
+        print("Elementos inicializados")
