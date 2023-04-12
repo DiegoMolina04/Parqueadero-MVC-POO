05a730dfffd47fa51ce6ecdbbb3c146c802dde75
Diego Molina <diegomolina_30@hotmail.com>
2023-04-11 23:07:44 -0500

    update Administrador with methods

--- src/mvc/model/Administrador.py
+++ src/mvc/model/Administrador.py
@@ -1,4 +1,8 @@
 from model.Persona import Persona
+from model.Parqueadero import Parqueadero
+from datetime import datetime
+#from controller.LoginController import LoginController
+import pandas
 
 class Administrador(Persona):
 
@@ -10,7 +14,7 @@ class Administrador(Persona):
         self.contraseña = contraseña
         Administrador.arrayAdministradores.append([self.cedula, self.nombre, self.correo, self.contraseña])
 
-    def iniciarSesion(correo, contraseña):
+    def iniciarSesion(correo:str, contraseña:str):
         
         repuesta = [False, None]
         
@@ -22,11 +26,48 @@ class Administrador(Persona):
         
         return repuesta
 
-    def registrarLlegada(self):
-        pass
+    def registrarLlegada(cedulaEntrada:int,nombre:str,placa:str,marca:str,modelo:str,color:str,espacioSeleccionado:str,horaLlegada:datetime):
 
-    def registrarSalida(self):
-        pass
+        espaciosDisponibles = pandas.DataFrame(Parqueadero.arrayEspacios)
 
-    def terminarSesion(self):
-        pass
+        for x in range(espaciosDisponibles.shape[0]):
+            for y in range(espaciosDisponibles.shape[1]):
+                
+                if espaciosDisponibles[y][x].get('Espacio') == espacioSeleccionado:
+                    #print("Este es el array de datos",espaciosDisponibles[y][x])
+                    Parqueadero.arrayEspacios[x][y] = {"Disponibilidad":False, "Espacio":espacioSeleccionado, "Cédula":cedulaEntrada,"Nombre":nombre, "Placa":placa, "Marca":marca, "Modelo":modelo, "Color":color, "Hora_Llegada":horaLlegada}
+                    print("Este es el array actualizado\n",Parqueadero.arrayEspacios)
+                    return True
+        
+        
+        return False
+        #return arrayRespuesta
+        
+        #Parqueadero.actualizarEspacio()       
+
+    def registrarSalida(cedulaSalida:int):
+        
+        arrayRespuesta =[False, None, None]
+        espaciosDisponibles = pandas.DataFrame(Parqueadero.arrayEspacios)
+
+        for x in range(espaciosDisponibles.shape[0]):
+            for y in range(espaciosDisponibles.shape[1]):
+                
+                if espaciosDisponibles[y][x].get('Cédula') == cedulaSalida:
+
+                    nombre = espaciosDisponibles[y][x].get('Nombre')
+                    hora = espaciosDisponibles[y][x].get('Hora_Llegada')
+                    espacio = espaciosDisponibles[y][x].get('Espacio')
+
+                    Parqueadero.arrayEspacios[x][y] = {"Disponibilidad":True, "Espacio":espacio, "Cédula":"","Nombre":"", "Placa":"", "Marca":"", "Modelo":"", "Color":"", "Hora_Llegada":""}
+
+                    arrayRespuesta = [True, nombre, hora]
+                    print("Este es el array salida\n",Parqueadero.arrayEspacios)
+                    return arrayRespuesta
+        
+        return arrayRespuesta
+
+    def terminarSesion():
+        from controller.LoginController import LoginController
+        loginController = LoginController()
+        loginController.iniciarLogin()
