b1dbb00883304323af1b345e0bed7bf9ae6e7a10
Diego Molina <diegomolina_30@hotmail.com>
2023-04-11 23:04:57 -0500

    update Cliente with arrayCliente & method realizarPago

--- src/mvc/model/Cliente.py
+++ src/mvc/model/Cliente.py
@@ -1,14 +1,22 @@
 from datetime import datetime
-from Persona import Persona
+from model.Persona import Persona
 
 class Cliente(Persona):
     
+    arrayClientes = []
+
     def __init__(self, hora:datetime, cedulaCliente: int, nombreCliente: str):
         super().__init__(cedulaCliente, nombreCliente)
         self.hora = hora
+        Cliente.arrayClientes.append([self.cedula, self.nombre, self.hora])
 
-    def realizarPago(self):
-        pass
+    def realizarPago(horaLlegada:datetime):
+        horaSalida = datetime.now()
+        tiempoTranscurrido = horaSalida-horaLlegada
+        valorCosto = 10
+        valorPagar = round((tiempoTranscurrido.total_seconds()*valorCosto),3)
+
+        return valorPagar
 
     def conducir(self):
         pass
