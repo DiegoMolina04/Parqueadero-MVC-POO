0c1ba7dbf716ea632abaa66f2907db3dccea73cd
Diego Molina <diegomolina_30@hotmail.com>
2023-04-11 23:03:33 -0500

    update Carro with new atribute cedulaDueño and arrayCarro

--- src/mvc/model/Carro.py
+++ src/mvc/model/Carro.py
@@ -1,10 +1,14 @@
 class Carro():
+    
+    arrayCarro = []
 
-    def __init__(self, placa:str, marca:str, modelo:str, color:str):
+    def __init__(self, cedulaDueño:int, placa:str, marca:str, modelo:str, color:str):
+        self.cedulaDueño = cedulaDueño
         self.placa = placa
         self.marca = marca
         self.modelo = modelo
         self.color = color
+        Carro.arrayCarro.append([self.cedulaDueño, self.placa, self.marca, self.modelo, self.color])
 
     def arrancar():
         pass
