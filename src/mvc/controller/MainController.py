ae537f804b03426d6c4e3b2a5ae2b0334ba2b9a0
Diego Molina <diegomolina_30@hotmail.com>
2023-05-05 17:44:12 -0500

    change how initialize classes

--- src/mvc/controller/MainController.py
+++ src/mvc/controller/MainController.py
@@ -1,9 +1,10 @@
-from model.Administrador import Administrador
-from model.Parqueadero import Parqueadero
+from model.Main import Main
 
 class MainController():
 
     def iniciarDatos():
         #admin1 = Administrador("correo@correo","1234",123456798,"Marcos")
         #admin2 = Administrador("a","a",78945,"Viviana")
-        parqueadero = Parqueadero(2, 5)
+        #Parqueadero(2, 5)
+        tamañoParqueadero = [2,5]
+        Main.iniciarModelo(tamañoParqueadero)
