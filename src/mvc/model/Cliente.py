70b77846693c835b1e287c5e56bd3e57528597d7
Diego Molina <diegomolina_30@hotmail.com>
2023-05-05 17:39:57 -0500

    update class Cliente realizaPago with bd

--- src/mvc/model/Cliente.py
+++ src/mvc/model/Cliente.py
@@ -11,15 +11,25 @@ class Cliente(Persona):
         Cliente.arrayClientes.append([self.cedula, self.nombre, self.hora])
 
     def realizarPago(horaLlegada:datetime):
+        
         horaSalida = datetime.now()
-        tiempoTranscurrido = horaSalida-horaLlegada
+        horaSalida = horaSalida.strftime('%Y-%m-%d %I:%M:%S') #Se toma la hora y se le da un formato en especifico como string
+        
+        horaLlegada = horaLlegada.replace('/', ' ') 
+        horaLlegada = horaLlegada.replace('+', ':') #Se modifica los signos de la horaLLegada para ser legible como hora
+
+        llegadaDt = datetime.strptime(horaLlegada, '%Y-%m-%d %I:%M:%S') #Se modifica hora llegada como tipo datetime
+        salidaDt = datetime.strptime(horaSalida, '%Y-%m-%d %I:%M:%S') #Se modifica hora salida como tipo datetime
+
+        tiempoTranscurrido = salidaDt-llegadaDt #Se saca la diferencia de tiempo
         valorCosto = 10
-        valorPagar = round((tiempoTranscurrido.total_seconds()*valorCosto),3)
+        
+        valorPagar = round((tiempoTranscurrido.total_seconds()*valorCosto),3) #Se multiplica la diferencia de tiempo por valorCosto y redondea el valor a 3 decimales
+
+        # locale.setlocale(locale.LC_MONETARY, 'es-CO') #Se indica cual es la moneda local -> Colombia
+        # valorPagar = locale.currency(valorPagar) #Se da formato de moneda al valorPagar
 
         return valorPagar
 
     def conducir(self):
         pass
-
-# dt = datetime.now()
-# now = dt.strftime('%H:%M:%S')
